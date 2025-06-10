c9be4d80e416dcd4956e187a34ae7dbec6a1290c
Yttehs <shetty.yttehs@outlook.com>
2025-06-10 22:56:37 +0800

    feat: create Settings screen

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,16 +1,9 @@
 import BasicScreen from "@/components/basic/BasicScreen";
-import IndexTopBar from "@/components/screens/index/IndexTopBar";
-import { useNavigation } from "expo-router";
 import { StyleSheet } from "react-native";
 
 export default function Index() {
-  const navigation = useNavigation();
-
   return (
     <BasicScreen style={styles.container}>
-      <IndexTopBar
-        onClickSettings={() => navigation.navigate("settings" as never)}
-      />
     </BasicScreen>
   );
 }
