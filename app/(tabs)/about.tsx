cc6f8f44cec3dbfea5605d4f84c745ad4cd2c3df
Yttehs <shetty.yttehs@outlook.com>
2025-06-10 22:53:34 +0800

    feat: impl About page

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,17 +1,16 @@
 import BasicScreen from "@/components/basic/BasicScreen";
-import { StyleSheet, Text } from "react-native";
+import AboutTopBar from "@/components/screens/about/AboutTopBar";
+import { StyleSheet } from "react-native";
 
 export default function About() {
   return (
     <BasicScreen style={styles.container}>
-      <Text>About</Text>
+      <AboutTopBar />
     </BasicScreen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
