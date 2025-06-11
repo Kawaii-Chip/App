1d54068a9b171ef3dd2fc8a18398172354be1304
Yttehs <shetty.yttehs@outlook.com>
2025-06-11 22:17:37 +0800

    chore: make device_name and product_id constants

--- data/cloud/FetchAllProperties.ts
+++ data/cloud/FetchAllProperties.ts
@@ -1,9 +1,9 @@
-import { ONENET_BASE_URL } from "@/constants/Config";
+import { ONENET_BASE_URL, ONENET_DEVICE_NAME, ONENET_PRODUCT_ID } from "@/constants/Config";
 import { AUTH_TOKEN } from "@/constants/Secret";
 import axios from "axios";
 
 export default async function fetchAllProperties() {
-  const url = `${ONENET_BASE_URL}/query-device-property?product_id=mmMJx639W9&device_name=ch32`;
+  const url = `${ONENET_BASE_URL}/query-device-property?product_id=${ONENET_PRODUCT_ID}&device_name=${ONENET_DEVICE_NAME}`;
 
   try {
       const response = await axios.get(url, {
