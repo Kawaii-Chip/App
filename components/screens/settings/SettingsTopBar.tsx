4227c057db0d23734b6db7d75121c8830f4ac11f
Yttehs <shetty.yttehs@outlook.com>
2025-06-11 13:24:30 +0800

    feat: impl back for SettingsTopBar

--- components/screens/settings/SettingsTopBar.tsx
+++ components/screens/settings/SettingsTopBar.tsx
@@ -1,7 +1,21 @@
+import TopBarButton from "@/components/basic/top-bar-inner/TopBarButton";
 import TopBar from "@/components/basic/TopBar";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SettingsTopBar() {
+  const navigation = useNavigation();
+
   return (
-    <TopBar title="Settings" />
+    <TopBar
+      title="Settings"
+      buttonListLeft={(color) => [
+        <TopBarButton
+          key={1}
+          icon={<MaterialIcons name="arrow-back" size={24} color={color} />}
+          onPress={() => navigation.goBack()}
+        />
+      ]}
+    />
   )   
 }
